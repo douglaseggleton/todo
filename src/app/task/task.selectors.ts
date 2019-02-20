5808bd5b5b5b1dbbf3d73f1b24357bf9e8e8c5b7
Douglas Eggleton <douglas.eggleton@outlook.com>
2019-02-20 09:52:03 -0500

    :sparkles: Sorting cards by date.

--- src/app/task/task.selectors.ts
+++ src/app/task/task.selectors.ts
@@ -2,6 +2,7 @@ import { createSelector, createFeatureSelector } from '@ngrx/store';
 import { adapter, State } from './task.reducer';
 import { TaskStatus } from './task-status.enum';
 import { TaskDueStatus } from './task-due.enum';
+import { Task } from './task.interface';
 import * as moment from 'moment';
 
 export const getTaskState = createFeatureSelector<State>('task');
@@ -26,8 +27,21 @@ export const calculateStatus = (date, today) => {
   }
 }
 
-export const selectAllTasksWithDueStatus = createSelector(
+export const selectTasksSortedByDate = createSelector(
   getAllTasks,
+  (tasks) => tasks.sort((a: Task, b: Task) => {
+    if (moment(a.date).isBefore(b.date)) {
+      return -1;
+    } else if (moment(b.date).isBefore(a.date)) {
+      return 1;
+    } else {
+      return 0;
+    }
+  })
+);
+
+export const selectAllTasksWithDueStatus = createSelector(
+  selectTasksSortedByDate,
   (tasks) => tasks.map((task) => ({
     ...task,
     due: calculateStatus(task.date, new Date()) 
